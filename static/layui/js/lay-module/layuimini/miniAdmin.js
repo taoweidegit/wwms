c4a40481d9adcc148a29d97d1aac00cc477f7b20
Your Name <you@example.com>
2024-02-15 00:27:16 +0800

    备件申请...

--- static/layui/js/lay-module/layuimini/miniAdmin.js
+++ static/layui/js/lay-module/layuimini/miniAdmin.js
@@ -95,7 +95,8 @@ layui.define(["jquery", "miniMenu", "element","miniTab", "miniTheme"], function 
             var href = 'static/layui/page/welcome-1.html'
 //            sessionStorage.setItem('layuiminiHomeHref', data.href);
             sessionStorage.setItem('layuiminiHomeHref', href);
-            $('#layuiminiHomeTabId').html('<span class="layuimini-tab-active"></span><span class="disable-close">' + data.title + '</span><i class="layui-icon layui-unselect layui-tab-close">ဆ</i>');
+//            $('#layuiminiHomeTabId').html('<span class="layuimini-tab-active"></span><span class="disable-close">' + data.title + '</span><i class="layui-icon layui-unselect layui-tab-close">ဆ</i>');
+            $('#layuiminiHomeTabId').html('<span class="layuimini-tab-active"></span><span class="disable-close">首页</span><i class="layui-icon layui-unselect layui-tab-close">ဆ</i>');
             sessionStorage.setItem('layuiminiHomeHref', href);
 //            $('#layuiminiHomeTabId').attr('lay-id', data.href);
             $('#layuiminiHomeTabIframe').html('<iframe width="100%" height="100%" frameborder="no" border="0" marginwidth="0" marginheight="0"  src="' + href + '"></iframe>');
